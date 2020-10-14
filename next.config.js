a1a9f8f1059a5a1b850593336b4a26d20432eb59
Horacio Herrera <hi@horacioh.com>
2020-10-14 18:35:59 +0200

    testing new config

--- next.config.js
+++ next.config.js
@@ -8,37 +8,45 @@ checkEnv({
   required: ['NEXT_PUBLIC_DEPLOYMENT_URL'],
 })
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const useURL = (path) =>
   `${process.env.NEXT_PUBLIC_DEPLOYMENT_URL}${path || ''}`
 
-module.exports = withPlugins([
-  withMdxEnhanced({
-    layoutPath: 'src/layouts',
-    defaultLayout: true,
-    fileExtensions: ['mdx'],
-    remarkPlugins: [
-      require('remark-slug'),
-      require('remark-footnotes'),
-      require('remark-code-titles'),
-      //require('@fec/remark-a11y-emoji')
-    ],
-    rehypePlugins: [require('mdx-prism')],
-    extendFrontMatter: {
-      process: (mdxContent, frontMatter) => {
-        const pagesDir = nodePath.resolve(__dirname, 'src/pages')
-        // Somehow the __resourcePath does not start with a /:
-        const path = ('/' + frontMatter.__resourcePath)
-          .replace(pagesDir, '')
-          .replace('.mdx', '')
-          .replace('.tsx', '')
-          .replace(/^\/index$/, '/')
-          .replace(/\/index$/, '')
-        return {
-          path,
-          url: useURL(path),
-          readingTime: readingTime(mdxContent),
-        }
+const nextConfig = {
+  assetPrefix: '',
+}
+module.exports = withPlugins(
+  [
+    withMdxEnhanced({
+      layoutPath: 'src/layouts',
+      defaultLayout: true,
+      fileExtensions: ['mdx'],
+      remarkPlugins: [
+        require('remark-slug'),
+        require('remark-footnotes'),
+        require('remark-code-titles'),
+        //require('@fec/remark-a11y-emoji')
+      ],
+      rehypePlugins: [require('mdx-prism')],
+      extendFrontMatter: {
+        process: (mdxContent, frontMatter) => {
+          const pagesDir = nodePath.resolve(__dirname, 'src/pages')
+          // Somehow the __resourcePath does not start with a /:
+          const path = ('/' + frontMatter.__resourcePath)
+            .replace(pagesDir, '')
+            .replace('.mdx', '')
+            .replace('.tsx', '')
+            .replace(/^\/index$/, '/')
+            .replace(/\/index$/, '')
+          return {
+            path,
+            url: useURL(path),
+            readingTime: readingTime(mdxContent),
+          }
+        },
       },
-    },
-  }),
-])
+    }),
+  ],
+  nextConfig,
+)
