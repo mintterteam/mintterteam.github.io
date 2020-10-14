582b996f2891d25d51d4eb2eb2039db5909f4ea9
Horacio Herrera <hi@horacioh.com>
2020-10-14 12:39:24 +0200

    remove home layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,11 @@ export default function Footer() {
   return (
     <footer className="text-gray-700 w-full flex justify-between items-center pt-24 text-sm">
       <p className="flex-1">Mintter Technologies</p>
+      <Link href="/">
+        <a className="px-2">Home</a>
+      </Link>
       <Link href="/hiring">
-        <a>We are Hiring!</a>
+        <a className="px-2">We are Hiring!</a>
       </Link>
     </footer>
   )
