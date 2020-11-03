71083113c1c972b6d212c183590d1809ce017d46
GaboHBeaumont <satoshi@gaboshi.local>
2020-11-03 14:51:48 +0100

    small changes to the text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className="text-gray-700 w-full flex justify-between items-center pt-24 text-sm">
-      <p className="flex-1">Mintter Technologies</p>
+      <p className="flex-1">Mintter Technologies S.L. 2020 Spain</p>
       <Link href="/">
         <a className="px-2">Home</a>
       </Link>
